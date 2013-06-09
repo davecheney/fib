67eda83be9cb1dc18d5cceaff900c01d8fd710fc
Dave Cheney <dave@cheney.net>
2013-06-09 11:50:29 +1000

    Added Fib tests

---
+++ fib_test.go
@@ -0,0 +1,25 @@
+package fib
+
+import "testing"
+
+var fibTests = []struct {
+	n        int
+	expected int
+}{
+	{1, 1},
+	{2, 1},
+	{3, 2},
+	{4, 3},
+	{5, 5},
+	{6, 8},
+	{7, 13},
+}
+
+func TestFib(t *testing.T) {
+	for _, tt := range fibTests {
+		actual := Fib(tt.n)
+		if actual != tt.expected {
+			t.Errorf("Fib(%d): expected %d, actual %d", tt.n, tt.expected, actual)
+		}
+	}
+}
