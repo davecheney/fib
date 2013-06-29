afc9f52bb6137f331f92553265f5a9cd4e4a146d
Dave Cheney <dave@cheney.net>
2013-06-29 22:11:10 +1000

    added final example

--- fib_test.go
+++ fib_test.go
@@ -36,3 +36,17 @@ func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
 func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
 func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
 func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
+
+var result int
+
+func BenchmarkFibComplete(i int, b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		// always record the result of Fib to prevent
+		// the compiler eliminating the function call.
+		r = Fib(i)
+	}
+	// always store the result to a package level variable
+	// so the compiler cannot eliminate the Benchmark itself.
+	result = int
+}
